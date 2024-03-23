d2276db4965f4b95707dbb9778058333fe7a0cf7
AdityaGaur7 <adgaur027@gmail.com>
2024-03-23 10:37:20 +0530

    ok

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Component } from '@/components/component/component'
+const page = () => {
+  return (
+    <div>
+      <Component/>
+    </div>
+  )
+}
+
+export default page
